1acb391bda3e3e1cae4f8ac6c0392f05e37def5b
unknown <noelcamay@gmail.com>
2023-03-20 11:05:59 +0800

    updates

--- wp-content/themes/carkeys/page.php
+++ wp-content/themes/carkeys/page.php
@@ -5,7 +5,7 @@
     <main id="page-content">
 		<?php if ( have_posts() ): ?>
 			<?php while ( have_posts() ): the_post(); ?>
-                <div class="page-wrapper<?php echo is_shop() ? ' container shop-inner' : '';?><?php echo is_cart() ? ' container cart-container' : '';?>">
+                <div class="page-wrapper<?php echo is_shop() ? ' container shop-inner' : '';?><?php echo is_cart() ? ' container cart-container' : '';?><?php echo is_account_page() ? ' container account-container' : '';?>">
                     <?php if( $post->ID == get_option('woocommerce_myaccount_page_id') ): do_action('account_breadcrumbs'); endif;?>
                 	<?php if( !is_shop() && !is_front_page() ):?>
                     	<h1><?php the_title() ?></h1>
